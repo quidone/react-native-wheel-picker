4931ba4fefa835664a632ddaa52f26fe543a8d05
rozhkovs <rozhkovse1@gmail.com>
2023-05-02 17:51:12 +0700

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+const prettierConfig = require('./.prettierrc.js');
+
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierConfig],
+  },
+  ignorePatterns: ['node_modules/', 'lib/'],
+};
