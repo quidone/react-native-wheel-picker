8910d03921b81c0bd02cfa07993bc9ede1ec73b1
shoma-mano <ms2geki@gmail.com>
2024-09-27 16:43:10 +0900

    fix: update react native version

--- example/src/pickers/customized/Overlay.tsx
+++ example/src/pickers/customized/Overlay.tsx
@@ -1,16 +1,12 @@
 import React, {memo} from 'react';
-import {StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import type {RenderOverlayProps} from '@quidone/react-native-wheel-picker';
 
 const Overlay = ({itemHeight, overlayItemStyle}: RenderOverlayProps) => {
   return (
     <View style={[styles.overlayContainer]} pointerEvents={'none'}>
       <View
-        style={[
-          styles.selection,
-          {height: itemHeight},
-          overlayItemStyle as StyleProp<ViewStyle>,
-        ]}
+        style={[styles.selection, {height: itemHeight}, overlayItemStyle]}
       />
     </View>
   );
