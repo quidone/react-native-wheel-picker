5edeca919c6382c5134e1fcdf50608e8f6663bef
Jeroen van Warmerdam <jeronevw@hotmail.com>
2025-01-11 19:28:17 +0100

    feat: add `testID` prop

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -5,7 +5,7 @@ import renderer from 'react-test-renderer';
 describe('WheelPicker', () => {
   it('should match snapshot', () => {
     const tree = renderer
-      .create(<WheelPicker data={[{value: 1, label: 'Item 1'}]} />)
+      .create(<WheelPicker data={[{value: 1, label: 'Item 1'}]} testID='wheel-picker' />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
