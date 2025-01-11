76dbc5b3f696f433aa7697f74d9806ae126df89d
Jeroen van Warmerdam <jeronevw@hotmail.com>
2025-01-11 19:25:43 +0100

    feat: add basic snapshot test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,12 @@
-it.todo('write a test');
+import WheelPicker from '@implementation/base';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+describe('WheelPicker', () => {
+  it('should match snapshot', () => {
+    const tree = renderer
+      .create(<WheelPicker data={[{value: 1, label: 'Item 1'}]} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
